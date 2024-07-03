1fd93e6fe61ed2d00a67ef577a72539cbe03e4b5
Asim Avhad <avhadasim30@gmail.com>
2024-07-03 15:46:02 +0530

    First Commit

---
+++ src/config/gemini.js
@@ -0,0 +1,32 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const apiKey = "AIzaSyDskzzp6mfR9NLilIKFALy_vlm4LB4HURc";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    // safetySettings,
+    // See https://ai.google.dev/gemini-api/docs/safety-settings
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  const response = result.response;
+  console.log(response.text());
+  return response.text();
+}
+
+export default run;
